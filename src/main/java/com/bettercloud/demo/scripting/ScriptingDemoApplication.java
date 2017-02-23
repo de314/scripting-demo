d4ac26b1e766a2edd150281a4258b89d523181b2
David Esposito <david.esposito@bettercloud.com>
2017-02-23 00:11:11 -0500

    Initial commit
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

---
+++ src/main/java/com/bettercloud/demo/scripting/ScriptingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bettercloud.demo.scripting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScriptingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptingDemoApplication.class, args);
+	}
+}
